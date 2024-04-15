f449bad626c2fb2566a815cdbd4563fed2dcfff4
Tukello Cristian <105870343+TukelloMathole@users.noreply.github.com>
2024-04-15 05:32:16 +0200

    Update DbContext.cs

--- Todo/Data/DbContext.cs
+++ Todo/Data/DbContext.cs
@@ -12,12 +12,10 @@ namespace Todo.Data
             _configuration = configuration;
         }
 
-        // Define DbSet for each entity (table) in your database
         public DbSet<TodoItem> TodoItems { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // Get the connection string from appsettings.json
             string connectionString = _configuration.GetConnectionString("DefaultConnection");
             optionsBuilder.UseSqlServer(connectionString);
         }
